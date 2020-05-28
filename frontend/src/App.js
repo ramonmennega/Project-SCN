a21f763f6a92cd97f2df012e44a664eff3ae9746
TKTurbo <t.kreder@student.alfa-college.nl>
2020-05-28 14:21:34 +0200

    Routing werkt eindelijk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
-    </div>
-  );
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <header>
+                    <li><Link to='/'> Home </Link></li>
+                    <li><Link to='/about'>About us</Link></li>
+                    <li><Link to='/projects'>Projects</Link></li>
+                    <li><Link to='/contact'>Contact</Link></li>
+                </header>
+                <Switch>
+                    <Route exact path='/'>
+                        <Home />
+                    </Route>
+                    <Route exact path='/about'>
+                        <About />
+                    </Route>
+                    <Route exact path='/projects'>
+                        <Projects />
+                    </Route>
+                    <Route exact path='/contact'>
+                        <Contact />
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
