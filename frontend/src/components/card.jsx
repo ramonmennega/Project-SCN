0cd5828414593b4bf2861e67ae4148b304f5ddc1
kianfoc <eKMk396pzEqM>
2020-06-30 11:13:37 +0200

    projecten ophalen

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import axios from "axios";
 
 import "../style/card.css";
 
 class Card extends React.Component {
+    state = {
+        projects: []
+      };
+
+    componentDidMount() {
+        axios.get('http://86.92.50.117:4000/projects')
+        .then(res => {
+            const projects = res.data;
+            this.setState({ projects });
+          })
+      }
     render() {
         return (
-            <div className={"card"}>
-                <h2 className={"cardHeader"}>{this.props.header}</h2>
-                <div className={"card-content"}>
-                    <p>{this.props.text}</p>
+            <div>
+                { this.state.projects.map(project => 
+                <div className={"card"}>
+                    <h2 className={"cardHeader"}>{project.name}</h2>
+                    <div className={"card-content"}>
+                        <p>{project.id}</p>
+                    </div>
                 </div>
+                )}
             </div>
         );
     }
